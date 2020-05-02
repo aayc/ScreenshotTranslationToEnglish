66d13d06d15a3f0fe1731ea37d7f5d2233c537d9
Aaron Chan <aaron.y.chan64@gmail.com>
2020-05-02 12:08:48 -0400

    Initial commit

---
+++ translate.py
@@ -0,0 +1,22 @@
+from PIL import Image
+from googletrans import Translator
+import pytesseract
+import pinyin
+import argparse
+
+parser = argparse.ArgumentParser(description="Translate an image of chinese text to its characters, pinyin and English")
+parser.add_argument("file", type=str)
+parser.add_argument("--src", default="chi_tra")
+args = parser.parse_args()
+
+lang = args.src
+translator = Translator()
+
+img = Image.open(args.file)
+config = "preserve_interword_spaces=1"
+characters = pytesseract.image_to_string(img, lang=lang, config=config)
+pinyin = pinyin.get(characters)
+english = translator.translate(characters).text
+print(characters)
+print(pinyin)
+print(english)
